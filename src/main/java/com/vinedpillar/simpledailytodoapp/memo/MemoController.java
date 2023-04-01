bb42941f4941830265b41744e5753e95cafda569
waldsteiner <waldsteiner@gmail.com>
2023-04-01 22:37:25 +0900

    #007 commit 업데이트 정보   - memo 패키지 클래스 작업 중   - mapper 클래스 내용 추가   - controller 추가 중

--- src/main/java/com/vinedpillar/simpledailytodoapp/memo/MemoController.java
+++ src/main/java/com/vinedpillar/simpledailytodoapp/memo/MemoController.java
@@ -1,13 +1,43 @@
 package com.vinedpillar.simpledailytodoapp.memo;
 
+import com.vinedpillar.simpledailytodoapp.dto.SingleResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class MemoController {
+    private final MemoService memoService;
+    private final MemoMapper memoMapper;
+
+    public MemoController(MemoService memoService, MemoMapper memoMapper) {
+        this.memoService = memoService;
+        this.memoMapper = memoMapper;
+    }
+
     // 댓글 생성 요청
+    @PostMapping
+    public ResponseEntity postMemo(@RequestBody MemoDto.Post requestBody) {
+        Memo memo = memoMapper.memoPostToMemo(requestBody);
+        Memo createMemo = memoService.createMemo(memo);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(memoMapper.memoPostToMemo(createMemo)), HttpStatus.CREATED);
+    }
+
 
     // 댓글 조회 요청
+    @GetMapping
+
 
     // 전체 댓글 조회 요청
+    @GetMapping
+
 
     // 댓글 수정 요청
+    @PatchMapping
+
 
     // 댓글 삭제 요청
+    @DeleteMapping("/{memo-id}")
+
 }
--- src/main/java/com/vinedpillar/simpledailytodoapp/memo/MemoMapper.java
+++ src/main/java/com/vinedpillar/simpledailytodoapp/memo/MemoMapper.java
@@ -2,10 +2,19 @@ package com.vinedpillar.simpledailytodoapp.memo;
 
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MemoMapper {
     // 메모 포스트 -> 메모
+    Memo memoPostToMemo(MemoDto.Post requestBody);
+
     // 메모 패치 -> 메모
+    Memo memoPatchToMemo(MemoDto.Patch requestBody);
+
     // 메모 -> 메모 반응
+    MemoDto.Response memoToMemoResponse(Memo memo);
+
     // 메모들 -> 메모 리스트
+    List<MemoDto.Response> memosToMemoResponseList(List<Memo> memoList);
 }
